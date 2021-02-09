fe1616f982d9417bdf1885a4826bfd8c0c5b5aec
Conner Tolley <jim.tolley@hcahealthcare.com>
2021-02-09 10:48:02 -0600

    finished with exercise

---
+++ README.md
@@ -0,0 +1,22 @@
+# Fetch Game
+Conner's attempt at solving the coding exercise for the test automation role at Fetch.
+
+## Requirements
+* Java
+* Maven
+* Git
+
+##Run
+*   Open a command line in the project root. 
+* Run command "mvn clean test -Dsurefire.suiteXmlFiles=testng.xml".
+
+## Install in Intellij IDE
+* Open existing project from "File at top left".
+* Navigate to FetchExerciseTests class in src/main/java/com/Local/Tests.
+* Right click on FetchExerciseTests on line 13 and click "Run".
+* If you get the message "TestNG by default disables loading DTD from unsecured Urls. If you need to explicitly load the DTD from a http url, please do so by using the JVM argument [-Dtestng.dtd.http=true]", click on Run > Edit Configurations, and add "-Dtestng.dtd.http=true" to the JVM argument.
+
+##Notes
+* The exercise runs a headless browser. If you would like to see the browser just comment out the line 
+"chrome_options.addArguments("--headless");"
+* I have the exercise set up to run 10 times in a row.
--- src/main/java/com/Local/Tests/FetchExerciseTests.java
+++ src/main/java/com/Local/Tests/FetchExerciseTests.java
@@ -1,6 +1,9 @@
+package com.Local.Tests;
+
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -8,7 +11,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class Scales {
+public class FetchExerciseTests {
 
     WebDriver driver;
     ArrayList<Integer> myInts;
@@ -31,12 +34,16 @@ public class Scales {
     @BeforeMethod
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
+        ChromeOptions chrome_options = new ChromeOptions();
+        chrome_options.addArguments("--headless");
+        driver = new ChromeDriver(chrome_options);
         driver.get("http://ec2-54-208-152-154.compute-1.amazonaws.com/");
     }
 
     @AfterMethod
     public void tearDown() {
+        myInts.clear();
+        fakeMap.clear();
         driver.quit();
     }
 
@@ -49,19 +56,16 @@ public class Scales {
 
                 // If we have two numbers and one is fake
                 if (!fakeMap.isEmpty()) {
-//                    if (findFakeBarNum(fakeMap)) {
-                        fakeMap.forEach((k,v) -> myInts.remove(k));
-                        fakeMap.forEach((k,v) -> myInts.remove(v));
-                        fakeMap.forEach((k,v) -> enterNums(k, myInts.get(0)));
-                        if (isFake()) {
-                            fakeMap.forEach((k,v) -> clickOnCoin(k));
-                        } else {
-                            fakeMap.forEach((k,v) -> clickOnCoin(v));
-                        }
-                        fakeMap.clear();
-                        break mainLoop;
+                    fakeMap.forEach((k, v) -> myInts.remove(k));
+                    fakeMap.forEach((k, v) -> myInts.remove(v));
+                    fakeMap.forEach((k, v) -> enterNums(k, myInts.get(0)));
+                    if (isFake()) {
+                        fakeMap.forEach((k, v) -> clickOnCoin(k));
+                    } else {
+                        fakeMap.forEach((k, v) -> clickOnCoin(v));
                     }
-//                }
+                    break mainLoop;
+                }
 
                 ArrayList<Integer> newInts = (ArrayList<Integer>) myInts.clone();
                 newInts.remove(i);
@@ -83,10 +87,6 @@ public class Scales {
                 }
             }
         }
-
-        System.out.println("stop");
-
-
     }
 
     private void enterNums(int i, int e) {
@@ -96,50 +96,14 @@ public class Scales {
         getWeighButton().click();
     }
 
-    private boolean findFakeBarNum(HashMap<Integer, Integer> fakeMap) {
-        for (Integer ii : fakeMap.keySet()) {
-            return clickOnCoin(ii);
-        }
-        return false;
-    }
-
-    private boolean clickOnCoin(Integer ii) {
+    private void clickOnCoin(Integer ii) {
+        System.out.println("Found it!");
+        System.out.println("The fake bar is '" + ii + "'.");
         WebElement coinButton = driver.findElement(By.id("coin_" + ii));
         coinButton.click();
-        System.out.println("Found it!");
         Alert alert = driver.switchTo().alert();
         Assert.assertTrue(StringUtils.contains(alert.getText(), "Yay! You find it!"), "The message is: " + alert.getText() + ".");
-        sleep();
         alert.accept();
-        return true;
-    }
-
-    private void sleep() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private Integer findFake(HashMap<Integer, Integer> fakeMap, ArrayList<Integer> myInts) {
-        return null;
-    }
-
-    private WebElement getRightSquare() {
-        return driver.findElement(By.id("right_0"));
-    }
-
-    private WebElement getLeftSquare() {
-       return driver.findElement(By.id("left_0"));
-    }
-
-    private WebElement getWeighButton() {
-        return driver.findElement(By.id("weigh"));
-    }
-
-    private WebElement getResetButton() {
-        return driver.findElement(By.xpath("//button[contains(text(), 'Reset')]"));
     }
 
     private boolean isFake() {
@@ -148,14 +112,29 @@ public class Scales {
         WebElement gameInfo = driver.findElement(By.className("game-info"));
         List<WebElement> resultList = gameInfo.findElements(By.cssSelector("ol > li"));
         String text = resultList.get(resultList.size() - 1).getText();
+        System.out.println(text);
 
         if (text.contains(">") || text.contains("<")) {
             fake = true;
-            System.out.println("It's a fake!");
-        } else {
-            System.out.println("Not a fake.");
         }
+
         return fake;
+    }
+
+    private WebElement getRightSquare() {
+        return driver.findElement(By.id("right_0"));
+    }
+
+    private WebElement getLeftSquare() {
+        return driver.findElement(By.id("left_0"));
+    }
+
+    private WebElement getWeighButton() {
+        return driver.findElement(By.id("weigh"));
+    }
+
+    private WebElement getResetButton() {
+        return driver.findElement(By.xpath("//button[contains(text(), 'Reset')]"));
     }
 
 }
